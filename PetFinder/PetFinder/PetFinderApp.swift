148cd9624ef6e67c8caced4f0cd5481b0dae8145
Nadezhda Zenkova <zenkova@domopult.ru>
2023-05-16 13:11:15 +0300

    Add PetFinder

---
+++ PetFinder/PetFinder/PetFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFinderApp.swift
+//  PetFinder
+//
+//  Created by Nadezhda Zenkova on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PetFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
